2acf663a94d2ef19209787552ea9a108592c8e81
zhengjushi <15706639196@163.com>
2019-05-28 00:57:02 +0800

    new

--- onlinevpsystem/src/main/java/com/gdss/onlinevpsystem/repository/CssRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gdss.onlinevpsystem.repository;
-
-import com.gdss.onlinevpsystem.entity.Css;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CssRepository extends JpaRepository<Css,Integer> {
-}
--- onlinevpsystem/src/main/java/com/gdss/onlinevpsystem/repository/HtmlRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gdss.onlinevpsystem.repository;
-
-import com.gdss.onlinevpsystem.entity.Html;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HtmlRepository extends JpaRepository<Html,Integer> {
-}
